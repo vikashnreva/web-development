a5866c709798a48e77bec34e840ef713bd60126f
vikashnreva <sweet.vikash.i@gmail.com>
2019-05-09 04:39:27 +0530

    Add files via upload

---
+++ rgb color game/colorGame.js
@@ -0,0 +1,29 @@
+
+var colors = [ 
+	"rgb(255,0,0)",
+	"rgb(0,255,0)",
+	"rgb(0,0,255)",
+	"rgb(0,255,255)",
+	"rgb(255,255,0)",
+	"rgb(255,0,255)"
+			]
+	
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorvar");
+colorDisplay.textContent = pickedColor;
+
+for(var i=0;i<squares.length;i++){
+	squares[i].style.background=colors[i];
+	squares[i].addEventListener("click", function()
+	{
+		var clickedColor = this.style.background;
+		if(clickedColor===pickedColor){
+			alert("correct");
+		}
+		
+		
+	});
+
+}
